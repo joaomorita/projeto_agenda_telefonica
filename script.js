059283160910d361d49f272a81369b630294900e
Morita <joao@teste.com>
2024-05-05 11:49:57 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById('btn-submit');
+let linhas = ''
+const telefones = []
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+
+    let nome = document.getElementById('nome-contato');
+    let telefone = document.getElementById('numero-telefone')
+    const corpoTabela = document.querySelector('tbody')
+
+    if(telefone.value == 0){
+        alert('Digite um telefone')
+    }else{
+        if(telefones.includes(telefone.value)){
+        alert('Esse telefone já foi adicionado na agenda')
+        }else{
+            telefones.push(telefone.value)
+            let linha = '<tr>'
+            linha += `<td>${nome.value}</td>`
+            linha += `<td>${telefone.value}</td>`
+            linha += '</tr>'
+            linhas += linha
+
+            corpoTabela.innerHTML = linhas
+        }
+    }
+})
